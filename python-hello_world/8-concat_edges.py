db1cc89e2a68c4770275e723cfa7b6bfafc50671
jrosmanov <cr.osmanov@gmail.com>
2025-11-29 16:45:42 +0000

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 str = str[34:51] + str[52:63] + str[64:75]
 print(str)
-
