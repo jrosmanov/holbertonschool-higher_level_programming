4785ba443900145ba64f2cda527d672f496c59c0
jrosmanov <cr.osmanov@gmail.com>
2025-12-01 15:10:31 +0000

    1.task

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
-def sub(a, b):
-    return a - b
-def mul(a, b):
-    return a * b
-def div(a, b):
-    return int(a / b)
+from calculator_1 import add, sub, mul, div
+
+
 def main():
     a = 10
     b = 5
@@ -14,5 +9,7 @@ def main():
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+
+
 if __name__ == "__main__":
     main()
