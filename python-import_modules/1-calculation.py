6c7cd8627488058d24627766356a1500dd98d0a5
jrosmanov <cr.osmanov@gmail.com>
2025-12-01 14:54:29 +0000

    1.task

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -7,9 +7,9 @@ def mul(a, b):
     return a * b
 def div(a, b):
     return int(a / b)
-a=10
-b=5
-print(f"{a} + {b} = {add(a, b)}")
-print(f"{a} - {b} = {sub(a, b)}")
-print(f"{a} * {b} = {mul(a, b)}")
-print(f"{a} / {b} = {div(a, b)}")
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
