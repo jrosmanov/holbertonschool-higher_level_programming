5bc6fde6d0e6ad663bccbc64139956ede913bca6
jrosmanov <cr.osmanov@gmail.com>
2025-12-01 14:51:04 +0000

    1.task

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+def mul(a, b):
+    return a * b
+def div(a, b):
+    return int(a / b)
+a=10
+b=5
+print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} - {b} = {sub(a, b)}")
+print(f"{a} * {b} = {mul(a, b)}")
+print(f"{a} / {b} = {div(a, b)}")
