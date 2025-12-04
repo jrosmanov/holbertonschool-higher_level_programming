214d77d017396dd280176083f68e45e7834e0a10
Jalal Osmanov <cr.osmanov@gmail.com>
2025-12-04 10:17:50 +0400

    Delete python-data_structures/7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def add_touple(touple_a=(), touple_b=()):
-    a1 = touple_a[0] if len(touple_a) > 0 else 0
-    a2 = touple_a[1] if len(touple_a) > 1 else 0
-    b1 = touple_b[0] if len(touple_b) > 1 else 0
-    b2 = touple_b[1] if len(touple_b) > 1 else 0
-    return (a1+b1, a2+b2)
