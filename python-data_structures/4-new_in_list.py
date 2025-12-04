480ae00c3ca0c283018631ebb54ba7c96c71145e
jrosmanov <cr.osmanov@gmail.com>
2025-12-04 05:33:13 +0000

    4.task

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,5 +3,5 @@ def new_in_list(my_list, idx, element):
     new_list = my_list[:]
     if idx < 0 or idx >= len(new_list):
         return new_list
-    new-list[idx] = element
+    new_list[idx] = element
     return new_list
