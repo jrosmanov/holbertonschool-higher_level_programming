de4e894eb30a292e9c620e8f1688cf2db33ac740
jrosmanov <cr.osmanov@gmail.com>
2025-12-08 09:30:02 +0000

    4.task

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    common = []
-    for x in set_1:
-        for y in set_2:
-            if x != y:
-                common.append(x)
-    return common
+    return (set_1 | set_2) - (set_1 & set_2)
