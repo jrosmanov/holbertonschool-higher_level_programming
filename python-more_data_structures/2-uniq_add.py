21c4af9fc0c9d4b7344f8b448cb50d9fa285bcef
jrosmanov <cr.osmanov@gmail.com>
2025-12-08 09:13:35 +0000

    2.task

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     result = 0
     uniq_list = list(set(my_list))
     for element in uniq_list:
-        result = result + uniq_list
+        result = result + element
     return result
