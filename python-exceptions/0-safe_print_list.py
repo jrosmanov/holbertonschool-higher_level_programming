2d62de196757c7050eabe28c2f6cabfd335dc009
jrosmanov <cr.osmanov@gmail.com>
2025-12-11 08:27:26 +0000

    0.Task

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        for i in range(x):
+            try:
+                print(my_list[i], end="")
+                printed += 1
+            except IndexError:
+                break
+    except Exception:
+        pass
+    print()
+    return printed
