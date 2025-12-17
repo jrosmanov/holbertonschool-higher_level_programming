47a3267e4398cff30c1e75be96e99bcf87fe0bd6
jrosmanov <cr.osmanov@gmail.com>
2025-12-17 11:56:00 +0000

    3.Task

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this will define empty square class """
+
+
+class Square:
+    """reprezents empty class"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size * self.__size
