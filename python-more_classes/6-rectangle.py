f39c135a3c83f944f5a80ed176a5e779a6bfd14a
jrosmanov <cr.osmanov@gmail.com>
2025-12-21 13:10:14 +0000

    6.Task

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -15,7 +15,8 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-
+    @width.setter
+    def width(self, value):
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value < 0:
