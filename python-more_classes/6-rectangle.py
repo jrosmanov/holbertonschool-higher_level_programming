ad80ad253a6b0da4659f7d7aa7e7ec794b3e2788
jrosmanov <cr.osmanov@gmail.com>
2025-12-21 13:12:21 +0000

    6.Task

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").__doc__)"""
+"""modul that represent class"""
 
 
 class Rectangle:
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
+    """the class that represent rectangle"""
 
     number_of_instances = 0
 
