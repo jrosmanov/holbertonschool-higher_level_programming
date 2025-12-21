3bead70b74cb0927b893a3bebefc6e354333f6a4
jrosmanov <cr.osmanov@gmail.com>
2025-12-21 13:29:35 +0000

    8.Task

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").__doc__)"""
+"""modul that represent class"""
 
 
 class Rectangle:
-    """python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
+    """the class that represent rectangle"""
 
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
-        python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'
-        
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
