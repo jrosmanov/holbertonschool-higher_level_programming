18a7144c04c60e459f8792f9b8e06aad13818a95
jrosmanov <cr.osmanov@gmail.com>
2025-12-21 13:08:07 +0000

    6.Task

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)"""
+
+
+class Rectangle:
+    """python3 -c 'print(__import__("my_module").MyClass.__doc__)"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __repr__(self):
+        return '<3-rectangle.Rectangle object at 0x7f92a75a2eb8>'
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
+
+        rectangle1 = []
+        for i in range(0, self.__height):
+            rectangle1.append('#' * self.__width)
+        return '\n'.join(rectangle1)
+
+    def __repr__(self):
+        return f'Rectangle({self.__width}, {self.__height})'
+
+    def __del__(self):
+        print('Bye rectangle...')
+        Rectangle.number_of_instances -= 1
