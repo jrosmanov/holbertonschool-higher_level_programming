cb141556eb494cfee0f4a1e44eee198a4f18a839
jrosmanov <cr.osmanov@gmail.com>
2025-12-25 12:36:28 +0000

    1.Task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating class"""
+
+
+class MyList(list):
+    """A class that inherits from the built-in list class."""
+    
+    def print_sorted(self):
+        print(sorted(self))
