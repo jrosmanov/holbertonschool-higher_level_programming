1ace6dbbf21ebaa33478019bd068a1c9dd219d56
jrosmanov <cr.osmanov@gmail.com>
2025-12-26 09:31:13 +0000

    6.Task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""creating class to define something"""
+
+
+class BaseGeometry:
+    """class that name is BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
