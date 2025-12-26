3e0cd97e13d3c1dc91567880d896bdcd661cd8d7
jrosmanov <cr.osmanov@gmail.com>
2025-12-26 09:24:16 +0000

    4.Task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """Check if an object is an instance of a class that inherited"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and not isinstance(obj, obj.__class__)
