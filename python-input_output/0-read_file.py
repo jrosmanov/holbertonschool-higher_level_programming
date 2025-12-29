84ea38b5d57a0dbbc52bafe122bacba3814d5d7f
jrosmanov <cr.osmanov@gmail.com>
2025-12-29 10:11:18 +0000

    0.Task

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""need modul is documented"""
+
+
 def read_file(filename=""):
+    """function is documented with this """
     with open (filename, "r") as file
         print(file.read())
