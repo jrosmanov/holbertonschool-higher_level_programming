bea192c5e7ff88f885c938ce28f7fb177d71e7f2
jrosmanov <cr.osmanov@gmail.com>
2025-12-30 14:56:34 +0000

    6.Task

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that documented"""
+import json
+
+
+def load_from_json_file(filename):
+    """documented function"""
+    
+    with open(filename, 'r') as file:
+        return json.load(file)
