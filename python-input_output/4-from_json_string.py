d034e1b964db35bd41f80b187d5b8dfa798cc4c7
jrosmanov <cr.osmanov@gmail.com>
2025-12-30 14:32:47 +0000

    4.Task

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""need modul is documented"""
+"""need modul is documented and long doc."""
+
+
 import json
-
-
 def from_json_string(my_str):
         """function is documented with this"""
     return json.loads(my_str)
