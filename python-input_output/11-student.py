70418cd4484d9eb098d2379d3cab9fec5016193c
jrosmanov <cr.osmanov@gmail.com>
2025-12-31 10:55:18 +0000

    11.Task

---
+++ python-input_output/11-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module documented"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__  # Return all attributes if no `attrs` list is provided
+        else:
+            # Return only the attributes in the `attrs` list
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key)
