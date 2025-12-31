c62ef0f24f886eb5fff255aaa3ce6803c91facf4
jrosmanov <cr.osmanov@gmail.com>
2025-12-31 11:06:54 +0000

    11.Task

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -10,12 +10,11 @@ class Student:
 
     def to_json(self, attrs=None):
         if attrs is None:
-            return self.__dict__  # Return all attributes if no `attrs` list is provided
+            return self.__dict__
         else:
-            # Return only the attributes in the `attrs` list
             return {key: value for key, value in self.__dict__.items()
                     if key in attrs}
 
     def reload_from_json(self, json):
-        for , value in json.items():
-            setattr(self, key)
+        for key, value in json.items():
+            setattr(self, key, value)
