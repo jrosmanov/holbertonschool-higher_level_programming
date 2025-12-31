9822cf3819faad93238aaab65c8ef3f3d2bdb9fa
jrosmanov <cr.osmanov@gmail.com>
2025-12-31 09:35:56 +0000

    7.Task

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module documented"""
+import sys
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_items_to_json():
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
+
+if __name__ == "__main__":
+    add_items_to_json()
