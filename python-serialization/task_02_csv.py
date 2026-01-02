8eb19be77481006dea96ff33cfb965c8255bfe9f
jrosmanov <cr.osmanov@gmail.com>
2026-01-02 10:02:05 +0000

    Task.02

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+
+    data = []
+
+    with open(csv_filename, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            data.append(row)
+
+    with open("data.json", "w") as json_file:
+        json.dump(data, json_file)
