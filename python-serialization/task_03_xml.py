d26967f590ad6d7b8ff347a2c9f0e80e0cfc5a6f
jrosmanov <cr.osmanov@gmail.com>
2026-01-03 09:48:17 +0000

    Task.03

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    root = ET.Element("data")
+
+    for key, value in dictionary.items():
+        child = ET.SubElement(root, key)
+        child.text = str(value)
+
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+    return True
