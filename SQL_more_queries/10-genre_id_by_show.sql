ea2d7149ad533e04a63b27bd16f6c8455541cdc4
jrosmanov <cr.osmanov@gmail.com>
2026-01-24 14:47:08 +0400

    task

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- sql file with no name
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id 
 FROM tv_shows JOIN tv_show_genres 
-ON tv_shows.id = tv_show_genres.show_id ORDER BY title, genre_id ASC; 
+ON tv_shows.id = tv_show_genres.show_id ORDER BY title, genre_id ASC;
