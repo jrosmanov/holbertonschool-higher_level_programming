779785c3d3c390c43982d871f6bd6e2abca2a04d
jrosmanov <cr.osmanov@gmail.com>
2026-01-24 14:28:03 +0400

    Task

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,3 +9,12 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database
+    )
+
