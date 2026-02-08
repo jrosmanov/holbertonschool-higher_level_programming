01b54e022b1da90a8c5efbc343815cdea07fe0a1
jrosmanov <cr.osmanov@gmail.com>
2026-02-08 23:28:27 +0400

    task

--- python-server_side_rendering/papers.py
+++ python-server_side_rendering/papers.py
@@ -1,4 +1,5 @@
 import json, csv
+import db
 
 def read_json_products():
     with open("products.json", "r") as f:
@@ -16,3 +17,18 @@ def read_csv_products():
                 "price": float(row["price"])
             })
     return products
+
+def read_database():
+    list_of_tables = ['id', 'name', 'category', 'price']
+    data = db.show()
+    products = []
+
+    for tup in data:
+        product_sample = {}
+        for index, i in enumerate(list_of_tables):
+            product_sample[i] = tup[index]
+        products.append(product_sample)
+
+    return products
+
+read_database()
